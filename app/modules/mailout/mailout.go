4b80b14044ec7ae2f8f944ff7d4d1b1c1cdcc0e0
André Charton <andre@charton.de>
2025-10-24 12:46:03 +0200

    feat: use oauth login with outlook - add methods

--- app/modules/mailout/mailout.go
+++ app/modules/mailout/mailout.go
@@ -17,6 +17,25 @@ func LoginAuth(username, password string) smtp.Auth {
     return &loginAuth{username, password}
 }
 
+
+func (a *loginAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+    return "LOGIN", []byte(a.username), nil
+}
+
+func (a *loginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+    if more {
+        switch string(fromServer) {
+        case "Username:":
+            return []byte(a.username), nil
+        case "Password:":
+            return []byte(a.password), nil
+        default:
+            return nil, errors.New("Unknown from server")
+        }
+    }
+    return nil, nil
+}
+
 func MailOut(content models.Report) {
 
 	// smtp server configuration.
